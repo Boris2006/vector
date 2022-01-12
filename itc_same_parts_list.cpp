c3a0e978ae7fdaf7c9898a6c8c238004631a1952
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2022-01-12 20:25:33 +0300

    Delete itc_same_parts_list.cpp

--- itc_same_parts_list.cpp
+++
@@ -1,9 +0,0 @@
-#include "easy_list.h"
-bool itc_same_parts_list(const vector <int> &a){
-    if (a.size() > 0){
-    for (int i = 1; i < a.size(); i ++){
-        if ((a[i] < 0 && a[i - 1] < 0) || (a[i] >= 0 && a[i - 1] >= 0))
-             return true;}}
-    return false;
-}
-
