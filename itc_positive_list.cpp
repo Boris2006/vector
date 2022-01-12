0b985b6baa247b2fc8dd34dcaf7a0df33aeee1d6
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2022-01-12 20:25:00 +0300

    Delete itc_positive_list.cpp

--- itc_positive_list.cpp
+++
@@ -1,9 +0,0 @@
-#include "easy_list.h"
-
-int itc_positive_list(const vector <int> &a){
-    int b = 0;
-    if (a.size() > 0){
-    for (int i = 0; i < a.size(); i ++){
-        if (a[i] > 0)
-             b ++;}}
-    return b;}
