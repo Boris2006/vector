c77434c499f8be9268d534f57a41820b7c1dd968
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2022-01-12 20:24:52 +0300

    Delete itc_lshift_list.cpp

--- itc_lshift_list.cpp
+++
@@ -1,8 +0,0 @@
-#include "easy_list.h"
-
-void itc_lshift_list(vector <int> &a){
-    int c = a[0];
-    if (a.size() > 0){
-    for (int i = 1 ; i < a.size(); i++){
-        a[i - 1] = a[i];}
-    a[a.size() - 1] = c;}}
