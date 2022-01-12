0d4bbfd855958113d75f148d4dc5eae4699e951b
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2022-01-12 20:25:15 +0300

    Delete itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++
@@ -1,9 +0,0 @@
-#include "easy_list.h"
-
-void itc_rev_par_list(vector <int> &a){
-    if (a.size() > 0){
-    	int c = 0;
-    	for (int i = 1 ; i < a.size(); i += 2){
-        	c = a[i];
-        	a[i] = a[i - 1];
-        	a[i - 1] = c;}}}
