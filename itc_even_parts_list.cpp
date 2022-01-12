acaceff11fc9f8dbac393702063b9f9cefe5673c
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2022-01-12 20:24:43 +0300

    Delete itc_even_parts_list.cpp

--- itc_even_parts_list.cpp
+++
@@ -1,8 +0,0 @@
-#include "easy_list.h"
-
-void itc_even_parts_list(const vector <int> &a, vector <int> &b){
-    if (a.size() > 0){
-    for (int i = 0; i < a.size(); i ++){
-        if (a[i] % 2 == 0)
-             b.push_back(a[i]);}
-    }}
