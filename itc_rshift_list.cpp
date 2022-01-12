16d032cd98036c80267c93d78923697d4b586fc3
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2022-01-12 20:25:24 +0300

    Delete itc_rshift_list.cpp

--- itc_rshift_list.cpp
+++
@@ -1,7 +0,0 @@
-#include "easy_list.h"
-void itc_rshift_list(vector <int> &a){
-    if (a.size() > 0){
-    int c = a[a.size() - 1];
-    for (int i = a.size() - 2; i >= 0; i--){
-        a[i + 1] = a[i];}
-    a[0] = c;}}
