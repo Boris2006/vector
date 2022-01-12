999eb133d8cf1d32106e56c874cc91d950992840
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2022-01-12 20:56:32 +0300

    Update min_char.cpp

--- min_char.cpp
+++ min_char.cpp
@@ -1,7 +1,7 @@
 #include "middle_list.h"
 int min_char(vector <int> &b){
-    int d = b[1];
-    for(int i = 0; i != b.size(); i++){
+    int d = b[0];
+    for(int i = 1; i != b.size(); i++){
          if (b[i] < d)
              d = b[i];}
     return d;}
