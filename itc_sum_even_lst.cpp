0530e24def4ee26fc6cf469d2d9f73c614b5b60a
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2022-01-20 11:14:31 +0200

    Update itc_sum_even_lst.cpp

--- itc_sum_even_lst.cpp
+++ itc_sum_even_lst.cpp
@@ -2,7 +2,7 @@
 long itc_sum_even_lst(const vector<int> &a)
 {
     long b = 0;
-    for (int i = 1; i < a.size(); i += 2)
+    for (int i = 0; i < a.size(); i += 2)
         b += a[i];
     return b;
 }
